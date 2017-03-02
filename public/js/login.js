de2989476512c9fa2d896dbfab03fb70583f287e
Dana Flint <Dana_Flint@msn.com>
2017-03-01 21:24:06 -0600

    adding login

---
+++ public/js/login.js
@@ -0,0 +1,72 @@
+var AUTH0_CLIENT_ID = 'vhEttCE1CDX47iLcwqrD9QzktODnIRSm';
+var AUTH0_DOMAIN = 'dflint1.auth0.com';
+var AUTH0_CALLBACK_URL = location.href;
+
+window.addEventListener('load', function() {
+    var lock = new Auth0Lock(AUTH0_CLIENT_ID, AUTH0_DOMAIN);
+
+    // var lock = new Auth0Lock('vhEttCE1CDX47iLcwqrD9QzktODnIRSm', 'dflint1.auth0.com');
+    document.getElementById('btn-login').addEventListener('click', function() {
+        lock.show();
+    });
+
+    document.getElementById('btn-logout').addEventListener('click', function() {
+        logout();
+    });
+
+    lock.on("authenticated", function(authResult) {
+        lock.getProfile(authResult.idToken, function(err, profile) {
+            if (err) {
+                // Remove expired token (if any)
+                localStorage.removeItem('id_token');
+                // Remove expired profile (if any)
+                localStorage.removeItem('profile');
+                return alert('There was an error getting the profile: ' + err.message);
+            } else {
+                localStorage.setItem('id_token', authResult.idToken);
+                localStorage.setItem('profile', JSON.stringify(profile));
+                showUserProfile(profile);
+            }
+        });
+    });
+
+    var parseHash = function() {
+        var id_token = localStorage.getItem('id_token');
+        if (null != id_token) {
+            var user_profile = JSON.parse(localStorage.getItem('profile'));
+            showUserProfile(user_profile);
+        } // else: not authorized
+    };
+
+    var showUserProfile = function(profile) {
+        // $('#login').style.display = "none";
+        // $('#logged').style.display = "inline-block";
+        // $('#avatar').src = profile.picture;
+        // $('#name').textContent = profile.name;
+        // $('#email').textContent = profile.email;
+        // $('#nickname').textContent = profile.nickname;
+        // $('#created_at').textContent = profile.created_at;
+        // $('#updated_at').textContent = profile.updated_at;
+        // $('#country').textContent = profile.country;
+
+        var user_info = ({
+                email: profile.email
+            });
+            //takes user email to post to page to 
+            //attach to our own db metadatha
+    //     $.post("/user", user_info).then((data) => {
+    //         console.log(data);
+    //         if (data.userType == "seeker") {
+    //             if (data.completedSurvey == false) {
+    //                 window.location.href = "/survey";
+    //             } else if (data.completedSurvey == true) {
+    //                 //if they are a seeker and have completed survey, direct them to documents portal
+    //                 window.location.href = "/document";
+    //             }
+    //         } else { //if user is a finder, directs them to list of surveys
+    //             window.location.href = "/surveyList";
+    //         }
+    //     });
+    };
+
+    // parseHash();
--- server.js
+++ server.js
@@ -1,10 +1,21 @@
+// *****************************************************************************
+// Server.js - This file is the initial starting point for the Node/Express server.
+//
+// ******************************************************************************
+// *** Dependencies
+// =============================================================
 var express = require("express");
 var bodyParser = require("body-parser");
 var methodOverride = require("method-override");
 
+
+
+// Sets up the Express App
+// =============================================================
 var port = process.env.PORT || 8080;
 
 var app = express();
+var db = require("./models");
 
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static(process.cwd() + "/public"));
@@ -22,9 +33,11 @@ app.set("view engine", "handlebars");
 
 // Import routes and give the server access to them.
 var routes = require("./controllers/findersfee_controller.js");
-
 app.use("/", routes);
 
+db.sequelize.sync({ force: true }).then(function() {
 app.listen(port, function() {
   console.log("App listening on PORT " + port);
 });
+});
+
